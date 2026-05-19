227c9cfd7a1de070ef00207a776119582f8c251e
Mohit Singh <167949839+swtmohit@users.noreply.github.com>
2026-05-19 12:12:13 +0530

    solve database issue.

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,19 @@
 import mongoose from "mongoose";
 
-export const connectionDB = async ():Promise<void> => {
+export const connectionDB = async (): Promise<void> => {
     const URI = process.env.MONGODB_URI;
-    const PORT = process.env.PORT;
-    if (!URI) throw new Error("MONGODB_URI not found in .env file");
+
+    if (!URI) {
+        throw new Error("MONGODB_URI not found in .env file");
+    }
 
     try {
-      await mongoose.connect(URI);
-        console.log(`MongoDB Database Connected ${PORT} `)
+        await mongoose.connect(URI);
+
+        console.log("✅ MongoDB Database Connected");
     } catch (err) {
-        console.error("Database Connection Field:", err)
+        console.error("Database Connection Failed:", err);
+
+        process.exit(1);
     }
-}
+};
